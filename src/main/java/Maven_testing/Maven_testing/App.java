44c7a59955f26a4ebe30f60f95481649bafceb0c
SanaSharminS <SanaSharminS.sanasharmint@gmail.com>
2021-10-26 14:39:01 +0530

    I did pushed first git code

---
+++ src/main/java/Maven_testing/Maven_testing/App.java
@@ -0,0 +1,13 @@
+package Maven_testing.Maven_testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
